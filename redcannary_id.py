378cb1575bc0c6a654faba331a66af2feaba027e
frack113 <62423083+frack113@users.noreply.github.com>
2022-06-25 09:56:55 +0200

    Add sigma rules check csv

--- redcannary_id.py
+++ redcannary_id.py
@@ -22,6 +22,8 @@ from collections import OrderedDict
 import time
 import re
 
+from dataclasses import dataclass
+
 yaml = YAML()
 yaml.preserve_quotes = True
 yaml.explicit_start = False
@@ -30,52 +32,90 @@ yaml.indent(sequence=4, offset=2)
 
 redcannary_info = my_data(yaml)
 
-print ('Build sigmahq dictionary')
-sigmahq_uuid = {}
-sigmahq_name = {}
-sigmahq_url ={}
-sigmahq_tag = {}
-sigmahq_files = pathlib.Path('../sigma/rules').glob('**/*.yml')
-for sigmahq_yml in sigmahq_files:
-    with sigmahq_yml.open('r',encoding="UTF-8") as file:
-        yml_sigma = yaml.load(file)
-        sigmahq_uuid[yml_sigma['id']] = sigmahq_yml.name
-        sigmahq_name[sigmahq_yml.name] = yml_sigma['id']
-        sigmahq_url[sigmahq_yml.name] = str(pathlib.PurePosixPath(sigmahq_yml)).replace('../sigma','https://github.com/SigmaHQ/sigma/tree/master')
-        if 'tags' in yml_sigma:
-            for tag in yml_sigma['tags']:
-                if re.match('attack.t\d+.*',tag):
-                    mitre = tag.replace('attack.t','T')
-                    if not mitre in sigmahq_tag:
-                        sigmahq_tag[mitre] = []
-                    sigmahq_tag[mitre].append(sigmahq_yml.name)
 
-need_to_download = False
+@dataclass
+class sigma:
+    uuid = {}
+    name = {}
+    url = {}
+    tag = {}
+    use = {}
 
-if pathlib.Path("index.yaml").exists():
-    epoch_now = time.time()
-    file_lstat = pathlib.Path("index.yaml").lstat()
-    delta_time = epoch_now-file_lstat.st_mtime
-    if  delta_time > 86400:
+@dataclass
+class csvdata:
+    header = [
+        "tactic",
+        "technique",
+        "executor",
+        "os",
+        "name",
+        "guid",
+        "sigma",
+        "nmr_test",
+    ]
+    data = [header]
+
+
+def load_sigma_yaml(path) -> sigma:
+    sigma_data = sigma()
+    sigmahq_files = pathlib.Path(path).glob("**/*.yml")
+
+    for sigmahq_yml in sigmahq_files:
+        with sigmahq_yml.open("r", encoding="UTF-8") as file:
+            yml_sigma = yaml.load(file)
+            sigma_data.uuid[yml_sigma["id"]] = sigmahq_yml.name
+            sigma_data.name[sigmahq_yml.name] = yml_sigma["id"]
+            sigma_data.url[sigmahq_yml.name] = str(
+                pathlib.PurePosixPath(sigmahq_yml)
+            ).replace("../sigma", "https://github.com/SigmaHQ/sigma/tree/master")
+
+            if "tags" in yml_sigma:
+                for tag in yml_sigma["tags"]:
+                    if re.match("attack.t\d+.*", tag):
+                        mitre = tag.replace("attack.t", "T")
+
+                        if not mitre in sigma_data.tag:
+                            sigma_data.tag[mitre] = []
+
+                        sigma_data.tag[mitre].append(sigmahq_yml.name)
+
+    sigma_data.use = {name: False for name in sigma_data.name.keys()}
+
+    return sigma_data
+
+
+def check_redcanary_yaml(path, delta):
+    need_to_download = False
+
+    if pathlib.Path(path).exists():
+        epoch_now = time.time()
+        file_lstat = pathlib.Path(path).lstat()
+        delta_time = epoch_now - file_lstat.st_mtime
+
+        if delta_time > delta:
+            need_to_download = True
+            pathlib.Path(path).unlink()
+
+    else:
         need_to_download = True
-        pathlib.Path("index.yaml").unlink()
-        
-        print (f"index.yaml is {delta_time}s old need to download")
 
-else:
-    need_to_download = True
+    if need_to_download:
 
-if need_to_download:
-    print("Download index.yaml")
+        my_file = requests.get(
+            "https://github.com/redcanaryco/atomic-red-team/raw/master/atomics/Indexes/index.yaml"
+        )
 
-    url = "https://github.com/redcanaryco/atomic-red-team/raw/master/atomics/Indexes/index.yaml"
-    my_file = requests.get(url)
+        with pathlib.Path(path).open("w", encoding="UTF-8", newline="\n") as file:
+            file.write(my_file.content.decode())
 
-    with pathlib.Path("index.yaml").open("w", encoding="UTF-8", newline="\n") as file:
-        file.write(my_file.content.decode())
 
-all_csv = [["tactic", "technique", "executor","os", "name", "guid", "sigma", "nmr_test"]]
-test_csv  = []
+sigma_data = load_sigma_yaml("../sigma/rules")
+check_redcanary_yaml("index.yaml", 86400)
+
+full_csv = csvdata()
+
+
+test_csv = []
 warning_log = []
 valid_guid = []
 
@@ -90,31 +130,31 @@ with pathlib.Path("index.yaml").open("r", encoding="UTF-8") as file:
 
             print(f"found {tactic} / {technique} : {nb_tests} tests")
 
-            if "technique" in yml_index[tactic][technique]:
-                technique_part = yml_index[tactic][technique]["technique"]
-                
-                head_info = {}
-                head_info["description"] = technique_part["description"]
-                head_info["name"] = technique_part["name"]
-                head_info["technique"] = []
+            head_info = {
+                "description": "",
+                "name": "",
+                "technique": [],
+                "tactic": [],
+            }
 
-                for ext_ref in technique_part["external_references"]:
+            if "technique" in yml_index[tactic][technique]:
+
+                head_info["description"] = yml_index[tactic][technique]["technique"][
+                    "description"
+                ]
+                head_info["name"] = yml_index[tactic][technique]["technique"]["name"]
+
+                for ext_ref in yml_index[tactic][technique]["technique"][
+                    "external_references"
+                ]:
                     if ext_ref["source_name"] == "mitre-attack":
                         head_info["technique"].append(ext_ref["external_id"])
 
-                head_info["tactic"] = []
-
-                for kill_ref in technique_part["kill_chain_phases"]:
+                for kill_ref in yml_index[tactic][technique]["technique"][
+                    "kill_chain_phases"
+                ]:
                     if kill_ref["kill_chain_name"] == "mitre-attack":
                         head_info["tactic"].append(kill_ref["phase_name"])
-
-            else:
-                head_info = {
-                    "description": "",
-                    "name": "",
-                    "technique": [],
-                    "tactic": [],
-                }
 
             if nb_tests > 0:
                 nmr_test = 0
@@ -132,24 +172,28 @@ with pathlib.Path("index.yaml").open("r", encoding="UTF-8") as file:
                         redcannary_info.load(yml_file)
 
                     redcannary_info.add(head_info, test)
-                    
-                    if len(redcannary_info.data['sigma_rule'])>0:
-                        for rule in redcannary_info.data['sigma_rule']:
-                            if rule['id'] in sigmahq_uuid:
-                                rule['name'] = sigmahq_uuid[rule['id']]
+
+                    if len(redcannary_info.data["sigma_rule"]) > 0:
+                        for rule in redcannary_info.data["sigma_rule"]:
+                            if rule["id"] in sigma_data.uuid:
+                                rule["name"] = sigma_data.uuid[rule["id"]]
+                                sigma_data.use[rule["name"]] = True
+
                             else:
-                                if rule["name"] in sigmahq_name:
-                                    rule["id"] = sigmahq_name[rule["name"]]
-                                    print(f'Fix found in {guid} file for Unidentified sigma id : {rule["id"]} / {rule["name"]}')
+                                if rule["name"] in sigma_data.name:
+                                    rule["id"] = sigma_data.name[rule["name"]]
+                                    sigma_data.use[rule["name"]] = True
+
+                                    
                                 else:
-                                    warn_str = f'No fix found in {guid} file for Unidentified sigma id : {rule["id"]} / {rule["name"]}'
-                                    print(warn_str)
-                                    warning_log.append(warn_str)
-                    
+                                    warning_log.append(
+                                        f'No fix found in {guid} file for Unidentified sigma id : {rule["id"]} / {rule["name"]}'
+                                    )
+
                     redcannary_info.order()
                     redcannary_info.save(yml_file)
 
-                    all_csv.append(
+                    full_csv.data.append(
                         [
                             tactic,
                             technique,
@@ -163,30 +207,45 @@ with pathlib.Path("index.yaml").open("r", encoding="UTF-8") as file:
                     )
 
             else:
-                if technique in sigmahq_tag:
-                    print(f"Found {len(sigmahq_tag[technique])} sigma rule(s)")
-                    info=[tactic,technique,','.join(sigmahq_tag[technique])]
+                if technique in sigma_data.tag:
+                    print(
+                        f"Found {len(sigma_data.tag[technique])} sigma rule(s)"
+                    )
+                    info = [
+                        tactic,
+                        technique,
+                        ",".join(sigma_data.tag[technique]),
+                    ]
                     test_csv.append(info)
 
 csv_file = pathlib.Path("Full_tests.csv")
 with csv_file.open("w", encoding="UTF-8", newline="\n") as csvfile:
     writer = csv.writer(csvfile, delimiter=";", quoting=csv.QUOTE_MINIMAL)
-    writer.writerows(all_csv)
+    writer.writerows(full_csv.data)
 
-if len(test_csv)>0:
+if len(test_csv) > 0:
     csv_file = pathlib.Path("missing_tests.csv")
+
     with csv_file.open("w", encoding="UTF-8", newline="\n") as csvfile:
         writer = csv.writer(csvfile, delimiter=";", quoting=csv.QUOTE_MINIMAL)
         writer.writerows(test_csv)
 
 if len(warning_log) > 0:
-    print('--------- Warning Found ---------')
+    print("--------- Warning Found ---------")
+
     for warn_srt in warning_log:
         print(warn_srt)
 
-print('--------- Check remove Test ---------')
-guid_files = pathlib.Path('yml/').glob('**/*.yml')
+print("--------- Check remove Test ---------")
+guid_files = pathlib.Path("yml/").glob("**/*.yml")
+
 for guid_file in guid_files:
     if guid_file.stem not in valid_guid:
         print(f"{guid_file.name} remove")
         guid_file.unlink()
+
+csv_file = pathlib.Path("sigma_rule.csv")
+with csv_file.open("w", encoding="UTF-8", newline="\n") as csvfile:
+    writer = csv.writer(csvfile, delimiter=";", quoting=csv.QUOTE_MINIMAL)
+    my_row= [[key,value] for key,value in sigma_data.use.items()]
+    writer.writerows(my_row)
